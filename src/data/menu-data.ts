ebc0f89b2ccf287e2b51fcc78cbededacca5db6e
roshan mef <roshan.mef@gmail.com>
2026-01-02 02:33:56 +0000

    Home About Us Services Leadership Milestones Gallery Downloads
    this is

--- src/data/menu-data.ts
+++ src/data/menu-data.ts
@@ -16,75 +16,11 @@ interface MenuItem {
 const menu_data: MenuItem[] = [
   { title: "Home", has_submenu: false, path: "/" },
   { title: "About Us", has_submenu: false, path: "/about-us" },
-  {
-    title: "Pages",
-    has_submenu: true,
-    path: "#",
-    sub_menus: [
-      {
-        title: "Team",
-        path: "#",
-        has_inner_submenu: true,
-        noBorder: true,
-        sub_menus: [
-          { title: "team", path: "/team" },
-          { title: "Team Details", path: "/single-team" },
-        ],
-      },
-      {
-        title: "Portfolio",
-        path: "#",
-        has_inner_submenu: true,
-        noBorder: true,
-        sub_menus: [
-          { title: "portfolio", path: "/portfolio" },
-          { title: "portfolio Details", path: "/single-portfolio" },
-        ],
-      },
-      {
-        title: "Service",
-        path: "#",
-        has_inner_submenu: true,
-        noBorder: true,
-        sub_menus: [
-          { title: "Service", path: "/service" },
-          { title: "Service Details", path: "/single-service" },
-        ],
-      },
-      {
-        title: "Utility",
-        path: "#",
-        has_inner_submenu: true,
-        noBorder: true,
-        sub_menus: [
-          { title: "faq", path: "/faq" },
-          { title: "pricing", path: "/pricing" },
-          { title: "404", path: "/errors-404" },
-        ],
-      },
-      {
-        title: "Account",
-        path: "#",
-        has_inner_submenu: true,
-        noBorder: true,
-        sub_menus: [
-          { title: "sign up", path: "/sign-up" },
-          { title: "sign in", path: "/sign-in" },
-          { title: "reset password", path: "/reset-password" },
-        ],
-      },
-    ],
-  },
-  {
-    title: "Blog",
-    has_submenu: true,
-    path: "#",
-    sub_menus: [
-      { title: "Blog", path: "/blog" },
-      { title: "Blog Details", path: "/single-blog" },
-    ],
-  },
-  { title: "Contact", has_submenu: false, path: "/contact-us" },
+  { title: "Services", has_submenu: false, path: "/service" },
+  { title: "Leadership", has_submenu: false, path: "/team" },
+  { title: "Milestones", has_submenu: false, path: "#" },
+  { title: "Gallery", has_submenu: false, path: "/portfolio" },
+  { title: "Downloads", has_submenu: false, path: "#" },
 ];
 
 
