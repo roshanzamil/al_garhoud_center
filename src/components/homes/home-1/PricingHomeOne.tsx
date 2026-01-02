b17e00d8a5488c2a17ac29a636d3ad0a2e4be6a1
roshan mef <roshan.mef@gmail.com>
2026-01-02 10:03:39 +0000

    remove above spacing for section Smart Government Solutions, Connecting

--- src/components/homes/home-1/PricingHomeOne.tsx
+++ src/components/homes/home-1/PricingHomeOne.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 export default function PricingHomeOne() {
   return (
-    <div className="azzle-section-padding2 position-r">
+    <div className="position-r pt-0">
       <div className="azzle-pricing-shape">
         <img src="assets/images/home1/pricing-shape.png" alt="bg" />
       </div>
