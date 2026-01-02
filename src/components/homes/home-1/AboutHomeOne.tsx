50d309628ff3ffc67fb75e96927a4b89c33637d5
roshan mef <roshan.mef@gmail.com>
2026-01-02 10:16:38 +0000

    no spacing still

--- src/components/homes/home-1/AboutHomeOne.tsx
+++ src/components/homes/home-1/AboutHomeOne.tsx
@@ -3,7 +3,7 @@
 export default function AboutHomeOne() {
   return (
     <>
-      <div className="azzle-section-padding pt-0 overflow-hidden">
+      <div className="azzle-section-padding overflow-hidden">
         <div className="container">
           <div className="row">
             <div className="col-lg-5 order-lg-2 azzle-about-thumb-container">
