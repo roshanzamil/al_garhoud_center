e3ca86306dcfa91c99ef8bf582bf559382d014eb
roshan mef <roshan.mef@gmail.com>
2026-01-03 03:23:16 +0000

    give a header

--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -9,6 +9,13 @@ export default function Gallery() {
       <HeaderOne />
       <div id="smooth-wrapper">
         <div id="smooth-content">
+          <section className="azzle-section-padding pb-0">
+            <div className="container">
+              <div className="azzle-section-title center max-width-780" data-aos="fade-up" data-aos-delay="500">
+                <h2>Our Gallery</h2>
+              </div>
+            </div>
+          </section>
           <GalleryArea />
           <FooterOne />
         </div>
