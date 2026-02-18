58d535b8982c96bb13db613aefd590f29d155d28
roshan mef <roshan.mef@gmail.com>
2026-02-18 11:10:36 +0000

    Leadership HomeLeadership Meet Our Leadership Thumb Mr. Abraham Maslo Ch

--- src/components/team/index.tsx
+++ src/components/team/index.tsx
@@ -2,8 +2,6 @@
 import FooterOne from '@/layouts/footers/FooterOne'
 import HeaderOne from '@/layouts/headers/HeaderOne'
 import Wrapper from '@/layouts/Wrapper' 
-import LeadershipArea from './TeamArea'
-import Breacrumb from '@/common/Breacrumb'
 import MessageFromLeaders from './MessageFromLeaders'
 
 export default function Leadership() {
@@ -12,8 +10,6 @@ export default function Leadership() {
       <HeaderOne />
       <div id="smooth-wrapper">
         <div id="smooth-content" style={{ paddingTop: '120px' }}>
-          <Breacrumb title="Leadership" page="Leadership" />
-          <LeadershipArea />
           <MessageFromLeaders />           
           <FooterOne />         
         </div>
